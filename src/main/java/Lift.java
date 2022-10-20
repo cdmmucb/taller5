5a746a867fc0015769c7eab65ed9961b63ce4cb1
cdmm359 <carlosmendizabal359@gmail.com>
2022-10-19 21:02:09 -0400

    clase person

--- src/main/java/Lift.java
+++ src/main/java/Lift.java
@@ -1,2 +1,3 @@
 public class Lift {
+    int piso = 1;
 }
--- src/test/java/LiftTest.java
+++ src/test/java/LiftTest.java
@@ -1,2 +1,32 @@
+import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
+
+import org.junit.rules.ExpectedException;
+
+import org.junit.Test;
+
+import org.junit.Before;
+
+
+
 public class LiftTest {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void liftComienzaPrimerPiso() throws Exception{
+        Lift lift = new Lift();
+        assertEquals(1,lift.piso);
+    }
+
+    @Test
+    public void crearPersona() throws Exception{
+        Person person = new Person();
+        assertEquals(person,new Person());
+    }
+
 }
